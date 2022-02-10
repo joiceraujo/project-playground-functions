c0fb4aadcca8879027550c6e4324ba1cbc0ac4ae
Joice Araújo <joicearaujoedu@gmail.com>
2022-02-10 02:07:08 -0300

    adicionado de 1 a 9

--- src/challenges.js
+++ src/challenges.js
@@ -4,47 +4,108 @@ function compareTrue(value1, value2) {
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return (base * height) / 2;
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+  return string.split(' ');
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(names) {
+  return ((names [names.length - 1 ]) + ', ' + names[0]);
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return wins * 3 + ties;
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(numeros) {
+  let maiorNumero = numeros[0];
+  let vezesApareceu = 0;
+
+  for (let i = 0; i < numeros.length; i += 1) {
+    if (numeros[i] > maiorNumero) {
+      maiorNumero = numeros[i];
+    }
+  }
+  for (let cont = 0; cont < numeros.length; cont += 1) {
+    if (maiorNumero == numeros[cont]) {
+      vezesApareceu += 1;
+    }
+  }
+  return vezesApareceu;
 }
 
+
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let aparecerMensagem;
+  let distanciaCat1 = mouse - cat1;
+  let distanciaCat2 = mouse - cat2;
+
+  if (distanciaCat1 < 0) {
+    distanciaCat1 = distanciaCat1 * (-1);
+
+  }else if (distanciaCat2 < 0) {
+    distanciaCat2 = distanciaCat2 * (-1);
+}
+
+if (distanciaCat1 > distanciaCat2) {
+  aparecerMensagem = 'cat2';
+} else if (distanciaCat2 > distanciaCat1){
+  aparecerMensagem = 'cat1';
+} else {
+  aparecerMensagem = 'os gatos trombam e o rato foge';
+}
+return aparecerMensagem;
+
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(numeros) {
+  let arrResultado = []
+
+  for (let i = 0; i < numeros.length; i +=1) {
+    if (numeros[i] % 3 == 0) {
+      arrResultado[i] = 'fizz';
+    }
+    if (numeros[i] % 5 == 0) {
+      arrResultado[i] = 'buzz';
+    }
+    if (numeros[i] % 3 == 0 && numeros[i] % 5 == 0) {
+      arrResultado[i] = 'fizzBuzz';
+    }
+    if (!(numeros[i] % 3 == 0) && !(numeros[i] % 5 == 0)) {
+      arrResultado[i] = 'bug!';
+    }
+  }
+  return arrResultado;
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(vog) {
+  let vocals = ['a', 'e', 'i', 'o', 'u'];
+  for (let index of vog) {
+    if (vocals.includes(index)) {
+      vog = vog.replace(index, vocals.indexOf(index) + 1);
+    }
+  } return vog;
 }
-function decode() {
-  // seu código aqui
-}
+encode('hello');
+function decode(phrase) {
+  let vocals = ['a', 'e', 'i', 'o', 'u'];
+  let Numbers = ['1', '2', '3', '4', '5'];
+  for (let index of phrase) {
+    if (Numbers.includes(index)) {
+      phrase = phrase.replace(index, vocals[index - 1]);
+    }  
+}return phrase;
+} 
 
 // Desafio 10
 function techList() {
